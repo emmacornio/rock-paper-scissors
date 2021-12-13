9ba7ad0cc42b2ee5462ab5f9a5caa18f7e40dbe5
emmacornio <emmacornio@protonmail.com>
2021-12-12 23:46:23 -0600

    add pseudo code

---
+++ script.js
@@ -0,0 +1,60 @@
+//declare player and computer choices as objects
+
+//2 parameters choice and value
+//make function for random selection
+//function called computerPlay
+//random number between 1 and 3
+//1=rock, 2=paper, 3=scissor
+//stores choice
+//log computer result
+//return choice
+//
+//make player selection function playerPlay
+//prompts to choose a choice
+//rock, paper,  scissors
+//has to be case insensitive
+//stores choice
+//returns choice
+//
+//round function 
+//calls for player and computer play
+//compare the values of the selections
+//log the result of the round
+//return 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
