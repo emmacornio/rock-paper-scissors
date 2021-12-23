254b5f0501a91e2bee23a171129683a5164fde2a
emmacornio <emmacornio@protonmail.com>
2021-12-22 19:32:54 -0600

    Add logit to play with ui

--- script.js
+++ script.js
@@ -1,209 +1,107 @@
-//declare player and computer choices as objects
-//2 parameters choice and value1
-const options = document.querySelectorAll(".options");
+const options = document.querySelectorAll(".option-image");
+const resultDiv = document.getElementById("result");
+const roundResult = document.getElementById("round-result");
+const counters = document.querySelectorAll(".counter");
+const optionText = document.querySelectorAll(".options");
+const gameResult = document.getElementById("game-result");
+const roundAnnouncement = document.getElementsByClassName("round");
 
 Array.from(options).forEach((option) => {
-	option.firstElementChild.addEventListener("click", () => {
-		console.log(option.textContent);
-	});
+	option.addEventListener("click", () => playRound(option.alt));
 });
+Array.from(optionText).forEach((option) => {
+	option.firstElementChild.lastElementChild.addEventListener("click", () =>
+		playRound(option.firstElementChild.lastElementChild.textContent)
+	);
+});
+function computerPlay() {
+	let computerNumber = Math.floor(Math.random() * 3 + 1);
+	let computerChoice;
 
-class choice {
-	constructor(selection, value) {
-		this.selection = selection;
-		this.value = value;
+	switch (computerNumber) {
+		case 1:
+			computerChoice = "Rock";
+			break;
+		case 2:
+			computerChoice = "Paper";
+			break;
+		case 3:
+			computerChoice = "Scissors";
+			break;
 	}
+
+	return computerChoice;
 }
 
-//make function for random selection
-//function called computerPlay
-let computerPlay = () => {
-	const computerChoice = new choice();
-	//random number between 1 and 3
-	computerChoice.value = Math.floor(Math.random() * 3 + 1);
-	//1=rock, 2=paper, 3=scissor
-	//stores choice
-	switch (computerChoice.value) {
-		case 1:
-			computerChoice.selection = "Rock";
-			break;
-		case 2:
-			computerChoice.selection = "Paper";
-			break;
-		case 3:
-			computerChoice.selection = "Scissors";
-			break;
-	}
-	//log computer result
-	//console.log(computerChoice.selection);
-	//return choice
-	return computerChoice;
-};
-//
-//make player selection function playerPlay
-let playerPlay = () => {
-	const playerChoice = new choice();
-	let str;
-	let option = false;
-	//prompts to choose a choice
-	//rock, paper,  scissors
-	while (option == false) {
-		str = prompt("Rock, Paper or Scissors?");
-		//has to be case insensitive
-		//stores choice
-		playerChoice.selection =
-			str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-		switch (true) {
-			case playerChoice.selection == "Rock":
-				playerChoice.value = 1;
-				option = true;
-				break;
-			case playerChoice.selection == "Paper":
-				playerChoice.value = 2;
-				option = true;
-				break;
-			case playerChoice.selection == "Scissors":
-				playerChoice.value = 3;
-				option = true;
-				break;
-			default:
-				alert("Enter a valid option");
-				option = false;
-		}
-	}
-	//returns choice
-	//console.log(playerChoice.selection);
-	return playerChoice;
-};
-//
-//
-//round function
-let playRound = () => {
-	//calls for player and computer play
-	let victory;
+function playRound(playerChoice) {
 	const computerChoice = computerPlay();
-	const playerChoice = playerPlay();
-	console.log(`player chose ${playerChoice.selection} | 
-				computer chose ${computerChoice.selection}`);
-	//compare the values of the selections
-	//log the result of the round
-	switch (playerChoice.value) {
-		case 1:
-			switch (computerChoice.value) {
-				case 1:
-					console.log(`It's a tie! ${playerChoice.selection} 
-							matches ${computerChoice.selection}`);
-					victory = 0;
-					break;
-				case 2:
-					console.log(
-						` You lose! ${computerChoice.selection}beats ${playerChoice.selection} `
-					);
-					victory = -1;
-					break;
-				case 3:
-					console.log(`You win! ${playerChoice.selection} beats 
-							${computerChoice.selection}`);
-					victory = 1;
-					break;
-			}
-			break;
-		case 2:
-			switch (computerChoice.value) {
-				case 1:
-					console.log(`You win! ${playerChoice.selection} 
-							beats ${computerChoice.selection}`);
-					victory = 1;
-					break;
-				case 2:
-					console.log(`It's a  tie! ${playerChoice.selection} 
-							matches ${computerChoice.selection}`);
-					victory = 0;
-					break;
-				case 3:
-					console.log(`You lose! ${computerChoice.selection} 
-							beats ${playerChoice.selection}`);
-					victory = -1;
-					break;
-			}
-			break;
-		case 3:
-			switch (computerChoice.value) {
-				case 1:
-					console.log(`You lose! ${computerChoice.selection} 
-							beats ${playerChoice.selection}`);
-					victory = -1;
-					break;
-				case 2:
-					console.log(`You win! ${playerChoice.selection} 
-							beats ${computerChoice.selection}`);
-					victory = 1;
-					break;
-				case 3:
-					console.log(`It's a tie!${playerChoice.selection} 
-							matches ${computerChoice.selection}`);
-					victory = 0;
-					break;
-			}
-			break;
-	}
-	// console.log(victory);
-	return victory;
-};
+	playerCounter = parseInt(counters[0].textContent);
+	computerCounter = parseInt(counters[1].textContent);
 
-//game function
-// let playGame = () => {
-// 	let victoryTally = 0;
-// 	//plays 5 rounds
-// 	for (let i = 0; i < 5; i++) {
-// 		victoryTally += playRound();
-// 		console.log("___________________________");
-// 		//console.log(victoryTally);
-// 		//recieves victory value each round
-// 	}
-// 	switch (true) {
-// 		//if at the end the value is positive the plaver wins
-// 		case victoryTally > 0:
-// 			console.log("Player Wins!");
-// 			break;
-// 		//if is negative the player looses
-// 		case victoryTally < 0:
-// 			console.log("Player Loses");
-// 			break;
-// 		//is is 0 is a match
-// 		case victoryTally === 0:
-// 			console.log("It's a match!");
-// 			break;
-// 	}
-// 	//at the end logs the winner
-// };
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+	console.log(
+		`player chose ${playerChoice} | computer chose ${computerChoice}`
+	);
+	roundAnnouncement[0].textContent = `player chose ${playerChoice} | computer chose ${computerChoice}`;
+
+	if (playerChoice == computerChoice) {
+		console.log("it's a tie!");
+		roundResult.textContent = "it's a tie!";
+	} else {
+		switch (true) {
+			case playerChoice == "Rock":
+				if (computerChoice == "Scissors") {
+					console.log("player wins!");
+					roundResult.textContent = "player wins!";
+					playerCounter += 1;
+				} else {
+					console.log("computer wins");
+					roundResult.textContent = "computer wins";
+					computerCounter += 1;
+				}
+
+				break;
+			case playerChoice == "Paper":
+				if (computerChoice == "Rock") {
+					console.log("player wins!");
+					roundResult.textContent = "player wins!";
+					playerCounter += 1;
+				} else {
+					console.log("computer wins");
+					roundResult.textContent = "computer wins";
+					computerCounter += 1;
+				}
+				break;
+			case playerChoice == "Scissors":
+				if (computerChoice == "Paper") {
+					console.log("player wins!");
+					roundResult.textContent = "player wins!";
+					playerCounter += 1;
+				} else {
+					console.log("computer wins");
+					roundResult.textContent = "computer wins";
+					computerCounter += 1;
+				}
+				break;
+		}
+		counters[0].textContent = playerCounter;
+		counters[1].textContent = computerCounter;
+	}
+	results = document.querySelectorAll(".result");
+
+	if (playerCounter < 5 && computerCounter < 5) {
+		gameResult.textContent = "Currently Playing";
+	}
+	if (playerCounter === 5 || computerCounter === 5) {
+		if (playerCounter === 5) {
+			gameResult.textContent = "Player Wins the Game!!!!";
+		} else {
+			gameResult.textContent = "Computer Wins the Game!!!";
+		}
+		counters[0].textContent = 0;
+		counters[1].textContent = 0;
+		playerCounter = 0;
+		computerCounter = 0;
+		roundResult.textContent = "";
+		roundAnnouncement.textContent = "";
+	}
+}
